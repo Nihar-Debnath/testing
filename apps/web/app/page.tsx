db0983b1d31ce2c78deb4b667d8c040e75b928ec
Nihar Debnath <nihardebnathmakaut@gmail.com>
2025-04-18 23:27:31 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -12,6 +12,7 @@ export default async function Home() {
       <div className="bg-white shadow-lg rounded-xl p-6 w-full max-w-md text-center">
         <h1 className="text-2xl font-bold text-gray-800">
           Welcome, {user?.username}!
+          Never Give up!!
         </h1>
         <div className="mt-4">
           <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
